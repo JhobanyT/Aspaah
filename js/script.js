057400d34da38b02044b86da158e4dfea15d0265
JhobanyT <jhobanyticona@gmail.com>
2021-07-03 17:55:38 -0500

    actualización

--- js/script.js
+++ js/script.js
@@ -1,13 +1,40 @@
-new Glider(document.querySelector('.slider__lista_imgvehiculos'), {
-    slidesToScroll: 1,
-    slidesToShow: 2,
-    draggable: true,
-    dots: '.indicadores',
-});
-new Glider(document.querySelector('.carousel__lista'), {
-    slidesToShow: 1,
-    arrows: {
-        prev: '.carousel__anterior',
-        next: '.carousel__siguiente'
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentScroll > 0){
+		window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll/15));
     }
-});
+}
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function(){
+
+	var scroll = document.documentElement.scrollTop || document.body.scrollTop;
+	if (scroll > 400){
+		buttonUp.style.transform = "scale(1)";
+	} else if(scroll < 400){
+		buttonUp.style.transform = "scale(0)";
+	}
+}
+
+
+var audio = document.getElementById('audio');
+var playPauseBTN = document.getElementById('playPauseBTN');
+var count = 0;
+
+function playPause(){
+	if(count == 0){
+		count = 1;
+		audio.play();
+		playPauseBTN.innerHTML = "🔊";
+	}else{
+		count = 0;
+		audio.pause();
+		playPauseBTN.innerHTML = "🔈";
+	}
+
+}
